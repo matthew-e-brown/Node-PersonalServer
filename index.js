05e71b43b2b638818176b596ae19ec8ef21b1e93
Matthew <matthew.e.brown.17@gmail.com>
2019-02-04 01:04:35 -0500

    Started attempting to port my site over to Node.js.
    Ran into issues with fs.statSync seeming to get stuck and hang forever.

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-const http = require("http");
+// https://www.youtube.com/playlist?list=PLRqwX-V7Uu6Yyn-fBtGHfN0_xCtBwUkBp
+// https://stackoverflow.com/questions/2727167/how-do-you-get-a-list-of-the-names-of-all-files-present-in-a-directory-in-node-j
+
 const express = require("express");
+const fs = require("fs");
+const parser = require("node-html-parser");
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send("Hello, World!");
+const server = app.listen(3000, () => {
+  console.log("listening on port:3000");
 });
 
-app.listen(3000);
+app.use(express.static("site"));
+
+app.get("/getsketches", (request, response) => {
+  const data = request.query;
+
+  fs.readdir("site/p5/", (err, blob) => {
+    if (err) throw err;
+    let reply = "";
+    for (let obj of blob) reply += `${obj} `
+    response.send(reply);
+  });
+
+});
