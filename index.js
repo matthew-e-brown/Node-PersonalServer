d4899ca03c822b585ce51e74975756fc7c4ceb31
Matthew Brown <matthew.e.brown.17@gmail.com>
2019-02-10 22:05:13 -0500

    Implemented GET route for getting code
    - Returns a JSON list of objects with:
       - the body of all the files in the requested directory,
       - the filenames,
       - and their extensions.
    - Also switched some other functions from let to const
    - Changed /getsketches to /getSketchList
    - /getSketchList now returns a path to the folder as well

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const server = app.listen(80, () => {
 
 app.use(express.static("public"));
 
-app.get("/getsketches", (request, response) => {
+app.get("/getSketchList", (request, response) => {
   let folderNames = [];
   let paths = [];
   fs.readdirSync("public/p5/").forEach((file) => {
@@ -33,6 +33,9 @@ app.get("/getsketches", (request, response) => {
     //Path to HTML File
     reply[i].pathToSrc = `${paths[i]}/${folderNames[i]}.html`;
 
+    //Path to Folder
+    reply[i].pathToFolder = paths[i];
+
     //Title
     let html = fs.readFileSync(`public/${reply[i].pathToSrc}`, "utf-8");
     html = htmlparse.parse(html);
@@ -43,7 +46,7 @@ app.get("/getsketches", (request, response) => {
       reply[i].desc = fs.readFileSync(`public/${paths[i]}/site-data/desc.txt`, "utf-8");
       // console.log(`${paths[i]} description check`);
     } catch (err) {
-      reply[i].desc = "";
+      reply[i].desc = "\n";
       // console.log(`${paths[i]} description failed`);
     }
 
@@ -58,7 +61,7 @@ app.get("/getsketches", (request, response) => {
       // console.log(`${paths[i]} dates check`);
     } catch (err) {
       reply[i].dateString = "";
-      reply[i].dateUnix = Number.MAX_SAFE_INTEGER;
+      reply[i].dateUnix = Number.MAX_SAFE_INTEGER; // If you can't find it, just stick it at the end of the list
       // console.log(`${paths[i]} dates failed`);
     }
   }
@@ -70,17 +73,17 @@ app.get("/getsketches", (request, response) => {
 });
 
 function dateFormatCustom(date) {
-  let n = date.getDate(); //day of month, 0 - 31
-  let m = date.getMonth(); // 0 - 11
-  let y = date.getFullYear(); // 2019
+  const n = date.getDate(); //day of month, 0 - 31
+  const m = date.getMonth(); // 0 - 11
+  const y = date.getFullYear(); // 2019
 
-  let months = [
+  const months = [
     'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
     'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
   ];
 
   return `${months[m]}. ${n}${((day) => {
-    let ld = day.toString().split('').pop(); //last digit
+    const ld = day.toString().split('').pop(); //last digit
     switch (ld) {
       case '1': return (day != 11) ? 'st' : 'th';
       case '2': return (day != 12) ? 'nd' : 'th';
@@ -89,3 +92,24 @@ function dateFormatCustom(date) {
     }
   })(n)}, ${y}`;
 }
+
+app.get("/getSourceCode", (request, response) => {
+  const folder = request.query.containingFolder;
+  const mainFile = request.query.mainFile; //The site can send the name of a mainFile
+  const reply = [];
+  fs.readdirSync(`public/${folder}`).forEach((file) => {
+    const stats = fs.statSync(`public/${folder}/${file}`);
+    const extension = file.split('.').pop();
+    if (stats.isFile() && extension.match(/js|html|css/)) {
+      const fileBody = fs.readFileSync(`public/${folder}/${file}`, "utf-8");
+      reply.push({
+        name: file,
+        body: (extension != "html") ? fileBody : fileBody.replace(/\</g, "&lt").replace(/\>/g, "&gt"),
+        extn: extension
+      });
+      if (file == mainFile || file == "sketch.js" || file == "index.html") reply[reply.length - 1].mainFile = true;
+      else reply[reply.length - 1].mainFile = false; //Tells whether the file is the one to be shown first
+    }
+  });
+  response.send(JSON.stringify(reply));
+});
