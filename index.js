8f213a243cfe90a90bf7319e92ee76af8f76f69c
Matthew Brown <matthew.e.brown.17@gmail.com>
2019-04-06 21:58:53 -0400

    Add 404 route
    - https://stackoverflow.com/questions/6528876/how-to-redirect-404-errors-to-a-page-in-expressjs
    - Not sure if the request.accepts('html') thing works.

--- index.js
+++ index.js
@@ -16,3 +16,16 @@ app.use(express.static("public"));
 /* Host my routes */
 app.use('/getSketchList', sketchlist);
 app.use('/getSourceCode', sourcecode);
+
+/* Since this is the last route, it will be the default
+ * in the event of a 404. Hence, it is the 404 route. */
+app.use((request, response, next) => {
+  response.status(404);
+
+  if (request.accepts('html')) {
+    response.redirect('404.html');
+    return;
+  }
+
+  response.type('txt').send('Error 404: Not Found.')
+});
